2d94a670946de1bfb334fc89b0464c8a929cb72f
Nathan D'Souza <85251596+nathandsouza10@users.noreply.github.com>
2021-12-27 22:16:15 +0000

    beginning of testing

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
+import time
 import matplotlib.pyplot as plt
 import numpy as np
 from binance import Client
 from sklearn import linear_model
 
-client = Client('zCVkQpBCxx9emHAjkhFXQRsz6PGdWPwLL9zT4UfkdL5D6HTXpiWaOFZULfqaci9M'
-                , 'xsP3n0q5GPvsL2N0WcdeSlznkTs8iQkNydusMKCrquSIlI2aXet9cKwyYvAFNk1R')
-print(client.get_asset_balance(asset='ZIL'))
+# real_account
+# client = Client('zCVkQpBCxx9emHAjkhFXQRsz6PGdWPwLL9zT4UfkdL5D6HTXpiWaOFZULfqaci9M'
+#                 , 'xsP3n0q5GPvsL2N0WcdeSlznkTs8iQkNydusMKCrquSIlI2aXet9cKwyYvAFNk1R')
 
-klines = client.get_historical_klines("ZILETH", Client.KLINE_INTERVAL_1MINUTE, "30 minutes ago GMT")
 
-prices = [float(x[4]) for x in klines]
-times = [x[6] for x in klines]
 
-times = [[int(x)] for x in times]
-prices = [float(x) for x in prices]
 
-data_X_train = times[:int(len(times) * 0.8)]
-data_X_test = times[int(len(times) * 0.8):]
+previous_time = time.time()
 
-data_Y_train = prices[:int(len(times) * 0.8)]
-data_Y_test = prices[int(len(times) * 0.8):]
+while True:
+    if time.time() > previous_time + (60*5):
+        # practice_account
+        client = Client('3zjhWnEBC0lBlfOeYxgnrGBQlnJj3Ci7ppnQnCC1v9CGQ7HqwnQlbXlKETZAZWUC'
+                        , 'XM52kHHwBiC8r88EFITS5WLVIMOOU1kHMcpLNV9EPgQHn69ctoY0uAIAWBQKmWfM')
+        client.API_URL = 'https://testnet.binance.vision/api'
+        print(client.get_asset_balance(asset='ETH'))
+        klines = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1MINUTE, "15 minutes ago GMT")
 
-model = linear_model.LinearRegression()
-model.fit(times, prices)
+        prices = [float(x[4]) for x in klines]
+        times = [x[6] for x in klines]
+        times = [[int(x)] for x in times]
+        prices = [float(x) for x in prices]
 
-print('coefficient', model.coef_)
-print('intercept', model.intercept_)
+        model = linear_model.LinearRegression()
+        model.fit(times, prices)
 
-x = np.linspace(times[0], times[-1])
-y = (model.coef_ * x) + model.intercept_
-plt.plot(times, prices, '-b')
-plt.plot(x, y, '-r')
-plt.show()
+        print('coefficient', model.coef_)
+        print('intercept', model.intercept_)
+
+        x = np.linspace(times[0], times[-1])
+        y = (model.coef_ * x) + model.intercept_
+        plt.scatter(times, prices)
+        plt.plot(x, y, color='green')
+        plt.show()
+        previous_time = time.time()
+
+        if model.coef_ > 0:
+            print('buy eth')
+        else:
+            print('sell eth')
