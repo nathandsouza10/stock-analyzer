f23946ba2b0c3ad5a600a6d313c36693e803a688
Nathan D'Souza <nathandsouza10@gmail.com>
2023-06-29 16:25:58 +0100

    included modern portfolio theory to get optimal portfolio weight

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from alpaca.data.requests import CryptoBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from datetime import datetime
 from utils import split_data
+import altair as alt
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -28,8 +29,9 @@ random.seed(42)
 st.title("Trading Bot (Day-to-Day)")
 
 client = CryptoHistoricalDataClient()
+STOCKS = ["BTC/USD", "ETH/USD", "LINK/USD", "SHIB/USD"]
 request_params = CryptoBarsRequest(
-                        symbol_or_symbols=["BTC/USD", "ETH/USD", "DAI/USD", "GRT/USD", "BTC/USD", "LINK/USD", "SHIB/USD"],
+                        symbol_or_symbols=STOCKS,
                         timeframe=TimeFrame.Day,
                         start=datetime(1600, 7, 1), #ridiculous datetime chosen to get earliest and latest possible stock price
                         end=datetime(2030, 9, 1)
@@ -61,6 +63,7 @@ x_test = torch.from_numpy(x_test).type(torch.Tensor).to(device)
 y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor).to(device)
 y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor).to(device)
 
+st.subheader("Models")
 with st.expander("LSTM Evaluation", expanded=True):
     with st.spinner("calculating trends..."):
         num_epochs = 1000
@@ -100,3 +103,39 @@ with st.expander("LSTM Evaluation", expanded=True):
             chart_data['y_test'] = np.squeeze(y_test)
             chart_data['y_test_pred'] = torch.squeeze(y_test_pred.cpu().detach())
             st.line_chart(chart_data, use_container_width=True)
+
+
+
+
+closing_price_df = pd.DataFrame()
+for stock in STOCKS:
+    closing_price_df[stock] = bars.df.loc[stock]['close']
+
+log_returns = np.log(closing_price_df / closing_price_df.shift(1))
+portfolio_returns = []
+portfolio_volatilities = []
+for x in range(1000):
+    weights = np.random.random(len(STOCKS))
+    weights /= np.sum(weights)
+    portfolio_returns.append(np.sum(weights * log_returns.mean()) * 365)
+    portfolio_volatilities.append(np.sqrt(np.dot(weights.T, np.dot(log_returns.cov() * 365, weights))))
+
+portfolio_returns = np.array(portfolio_returns)
+portfolio_volatilities = np.array(portfolio_volatilities)
+
+portfolios = pd.DataFrame({'Return': portfolio_returns, 'Volatility': portfolio_volatilities})
+st.subheader("modern portfolio theory")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.write("portfolio data")
+    st.dataframe(portfolios, use_container_width=True)
+
+with col2:
+    st.write("portfolio graph")
+    chart = alt.Chart(portfolios).mark_circle().encode(
+        x='Volatility',
+        y='Return'
+    ).interactive()
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+
